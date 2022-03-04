a6ad72fe77c76da1f56b20609b1335e7db20268c
winston <sheanwinston@gmail.com>
2022-03-04 11:44:51 +0100

    restart

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,5 +1,5 @@
 <script setup>
-import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
+import BreezeAuthenticatedLayout from '@/Layouts/Authenticated';
 import { Head } from '@inertiajs/inertia-vue3';
 </script>
 
