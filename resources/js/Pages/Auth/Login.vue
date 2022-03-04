4572162e1fd0d4fdc00fe4205ca5e5649e4a4280
winston <sheanwinston@gmail.com>
2022-03-04 13:21:31 +0100

    new setup

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,67 +1,124 @@
 <script setup>
-import BreezeButton from '@/Components/Button.vue';
-import BreezeCheckbox from '@/Components/Checkbox.vue';
-import BreezeGuestLayout from '@/Layouts/Guest.vue';
-import BreezeInput from '@/Components/Input.vue';
-import BreezeLabel from '@/Components/Label.vue';
-import BreezeValidationErrors from '@/Components/ValidationErrors.vue';
-import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
+    import BreezeButton from '@/Components/Button.vue';
+    import BreezeCheckbox from '@/Components/Checkbox.vue';
+    import BreezeGuestLayout from '@/Layouts/Guest.vue';
+    import BreezeInput from '@/Components/Input.vue';
+    import BreezeLabel from '@/Components/Label.vue';
+    import BreezeValidationErrors from '@/Components/ValidationErrors.vue';
+    import {
+        Head,
+        Link,
+        useForm
+    } from '@inertiajs/inertia-vue3';
 
-defineProps({
-    canResetPassword: Boolean,
-    status: String,
-});
-
-const form = useForm({
-    email: '',
-    password: '',
-    remember: false
-});
-
-const submit = () => {
-    form.post(route('login'), {
-        onFinish: () => form.reset('password'),
+    defineProps({
+        canResetPassword: Boolean,
+        status: String,
     });
-};
+
+    const form = useForm({
+        email: '',
+        password: '',
+        remember: false
+    });
+
+    const submit = () => {
+        form.post(route('login'), {
+            onFinish: () => form.reset('password'),
+        });
+    };
+
 </script>
 
 <template>
     <BreezeGuestLayout>
+
         <Head title="Log in" />
 
         <BreezeValidationErrors class="mb-4" />
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <div class="login-reg-bg">
+                <div class="log-reg-area sign">
+                    <h2 class="log-title">Login</h2>
+                    <p>
+                        Don’t use Winku Yet? <a href="#" title="">Take the tour</a> or <a href="#" title="">Join
+                            now</a>
+                    </p>
+                    <form method="post">
+                        <div class="form-group">
+                            <input type="text" id="input" required="required" />
+                            <label class="control-label" for="input">Username</label><i class="mtrl-select"></i>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" required="required" />
+                            <label class="control-label" for="input">Password</label><i class="mtrl-select"></i>
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" checked="checked" /><i class="check-box"></i>Always
+                                Remember Me.
+                            </label>
+                        </div>
+                        <a href="#" title="" class="forgot-pwd">Forgot Password?</a>
+                        <div class="submit-btns">
+                            <button class="mtr-btn signin" type="button"><span>Login</span></button>
+                            <button class="mtr-btn signup" type="button"><span>Register</span></button>
+                        </div>
+                    </form>
+                </div>
+                <div class="log-reg-area reg">
+                    <h2 class="log-title">Register</h2>
+                    <p>
+                        Don’t use Winku Yet? <a href="#" title="">Take the tour</a> or <a href="#" title="">Join
+                            now</a>
+                    </p>
+                    <form method="post">
+                        <div class="form-group">
+                            <input type="text" required="required" />
+                            <label class="control-label" for="input">First & Last Name</label><i
+                                class="mtrl-select"></i>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" required="required" />
+                            <label class="control-label" for="input">User Name</label><i class="mtrl-select"></i>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" required="required" />
+                            <label class="control-label" for="input">Password</label><i class="mtrl-select"></i>
+                        </div>
+                        <div class="form-radio">
+                            <div class="radio">
+                                <label>
+                                    <input type="radio" name="radio" checked="checked" /><i class="check-box"></i>Male
+                                </label>
+                            </div>
+                            <div class="radio">
+                                <label>
+                                    <input type="radio" name="radio" /><i class="check-box"></i>Female
+                                </label>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" required="required" />
+                            <label class="control-label" for="input"><a
+                                    href="http://www.wpkixx.com/cdn-cgi/l/email-protection" class="__cf_email__"
+                                    data-cfemail="692c0408000529">[email&#160;protected]</a></label><i
+                                class="mtrl-select"></i>
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" checked="checked" /><i class="check-box"></i>Accept Terms
+                                & Conditions ?
+                            </label>
+                        </div>
+                        <a href="#" title="" class="already-have">Already have an account</a>
+                        <div class="submit-btns">
+                            <button class="mtr-btn signup" type="button"><span>Register</span></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-
-        <form @submit.prevent="submit">
-            <div>
-                <BreezeLabel for="email" value="Email" />
-                <BreezeInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus autocomplete="username" />
-            </div>
-
-            <div class="mt-4">
-                <BreezeLabel for="password" value="Password" />
-                <BreezeInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <BreezeCheckbox name="remember" v-model:checked="form.remember" />
-                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    Forgot your password?
-                </Link>
-
-                <BreezeButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
-                </BreezeButton>
-            </div>
-        </form>
     </BreezeGuestLayout>
 </template>
